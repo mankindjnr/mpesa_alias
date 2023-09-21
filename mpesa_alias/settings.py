a8738f051e8077a5dd78a3a7165619af0a1c8247
mankindjnr <kamos@stu.kemu.ac.ke>
2023-09-21 14:59:12 +0300

    django secret

--- mpesa_alias/settings.py
+++ mpesa_alias/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("django_secret")
+SECRET_KEY = 'django-insecure-l5gs0pd0d$-t_bd#9&0n-e%1-v_=ub!^##n7s_hbib#wt&)aa_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
